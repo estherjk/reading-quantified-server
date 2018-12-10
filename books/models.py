71f2e29e304d8cea5137b089dbc3fbcfa2868b1d
Esther Jun Kim <hello@esthermakes.tech>
2018-12-09 18:41:07 -0800

    Update genre name to be a unique field
    This ensures we don't have duplicate genre names.

--- books/models.py
+++ books/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Genre(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    name = models.CharField(max_length=20)
+    name = models.CharField(max_length=20, unique=True)
 
     def __str__(self):
         return '%s' % (self.name)
