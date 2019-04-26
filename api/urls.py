4eccd69f4c8b8ef6a22e0e98614ac2b20ea46392
Esther Jun Kim <hello@esthermakes.tech>
2019-04-25 19:04:54 -0700

    Add Token Verify View

--- api/urls.py
+++ api/urls.py
@@ -3,6 +3,7 @@ from rest_framework import routers
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
+    TokenVerifyView,
 )
 
 from books.views import BookViewSet, GenreViewSet
@@ -17,5 +18,6 @@ urlpatterns = [
     url(r'^', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^token/$', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    url(r'^token/refresh/$', TokenRefreshView.as_view(), name='token_refresh')
+    url(r'^token/refresh/$', TokenRefreshView.as_view(), name='token_refresh'),
+    url(r'^token/verify/$', TokenVerifyView.as_view(), name='token_verify'),
 ]
