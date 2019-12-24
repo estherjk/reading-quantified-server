21a60fe4240615d7ce604a51df25c4226aa93781
Esther Jun Kim <hello@esthermakes.tech>
2019-12-24 09:41:11 -0800

    Update cover attachment field to be blank

--- web/books/models.py
+++ web/books/models.py
@@ -18,4 +18,4 @@ class Book(models.Model):
     date_started = models.DateTimeField()
     date_finished = models.DateTimeField()
     genres = models.ManyToManyField(Genre)
-    cover_attachment = JSONField(default=dict, help_text='Trello card attachment info. See https://developers.trello.com/reference#cardsidattachments.')
+    cover_attachment = JSONField(default=dict, blank=True, help_text='Trello card attachment info. See https://developers.trello.com/reference#cardsidattachments.')
